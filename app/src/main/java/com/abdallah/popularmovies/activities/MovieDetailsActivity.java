664388f31395f59fd779831c832242a99ab60f4e
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2018-07-09 21:10:40 +0200

    Remove unused drawable & optimize updates

--- app/src/main/java/com/abdallah/popularmovies/activities/MovieDetailsActivity.java
+++ app/src/main/java/com/abdallah/popularmovies/activities/MovieDetailsActivity.java
@@ -6,7 +6,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.widget.NestedScrollView;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -16,7 +15,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
-import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
